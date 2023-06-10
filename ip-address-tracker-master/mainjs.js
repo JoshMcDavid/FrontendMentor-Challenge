fca5de4be0a942daeb652b2827b96a5955ef8db1
Olorunshola Joshua <Olorunshola.J@bullseye.local>
2023-06-10 11:42:41 +0000

    frontend

---
+++ ip-address-tracker-master/mainjs.js
@@ -0,0 +1,13 @@
+
+
+const inputIP = document.querySelector(".input_ip");
+const buttonSubmit = document.querySelector(".btn");
+const userIP = document.querySelector(".ip_result");
+const userLocation = document.querySelector(".user-location");
+const userTimezone = document.querySelector(".user-timezone");
+const userServiceName = document.querySelector(".user-service-name");
+const userServiceValue = document.querySelector(".user-service-value");
+const modal = document.querySelector(".header_image");
+const svg = document.querySelector(".image");
+
+
